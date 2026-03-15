1645b301bfd2cc74080c4f4b5199be8eb2daa84e
William Wolfram <willwolfram18@gmail.com>
2026-03-14 22:36:21 -0700

    Cleanup items

--- src/test/kotlin/com/willwolfram18/extensions/kotlinlogging/CachedRegexMessageTemplateParserTests.kt
+++ src/test/kotlin/com/willwolfram18/extensions/kotlinlogging/CachedRegexMessageTemplateParserTests.kt
@@ -1,7 +1,5 @@
 package com.willwolfram18.extensions.kotlinlogging
 
-import com.willwolfram18.extensions.kotlinlogging.*
-
 class CachedRegexMessageTemplateParserTests : MessageTemplateParserTestBase() {
     override val parser: MessageTemplateParser = CachedRegexMessageTemplateParser()
 }
--- src/test/kotlin/com/willwolfram18/extensions/kotlinlogging/StringIterationTemplateParserTests.kt
+++ src/test/kotlin/com/willwolfram18/extensions/kotlinlogging/StringIterationTemplateParserTests.kt
@@ -1,7 +1,5 @@
 package com.willwolfram18.extensions.kotlinlogging
 
-import com.willwolfram18.extensions.kotlinlogging.*
-
 class StringIterationTemplateParserTests : MessageTemplateParserTestBase() {
     override val parser: MessageTemplateParser = StringIterationTemplateParser()
 }
