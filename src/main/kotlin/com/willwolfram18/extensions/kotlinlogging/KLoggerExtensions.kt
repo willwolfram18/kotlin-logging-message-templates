d0121a80ae2aa3206c9bd4ddd64b3cca4d94eaea
William Wolfram <willwolfram18@gmail.com>
2026-03-25 23:15:59 -0700

    Finished kotlin logging template methods

--- src/main/kotlin/com/willwolfram18/extensions/kotlinlogging/KLoggerExtensions.kt
+++ src/main/kotlin/com/willwolfram18/extensions/kotlinlogging/KLoggerExtensions.kt
@@ -3,7 +3,7 @@ package com.willwolfram18.extensions.kotlinlogging
 import io.github.oshai.kotlinlogging.KLogger
 
 var parser: MessageTemplateParser? = null
-private fun parserInstance(): MessageTemplateParser = parser ?: RegexMessageTemplateParser()
+private fun parserInstance(): MessageTemplateParser = parser ?: StringIterationTemplateParser()
 
 fun KLogger.debugTemplate(messageTemplate: String, vararg args: Any) = atDebugTemplate(null, messageTemplate, *args)
 
@@ -22,5 +22,35 @@ fun KLogger.infoTemplate(throwable: Throwable, messageTemplate: String, vararg a
 private fun KLogger.atInfoTemplate(throwable: Throwable?, messageTemplate: String, vararg args: Any) = atInfo {
     message = messageTemplate
     cause = throwable
-    payload = parserInstance().parseTemplateArguments(messageTemplate, args)
+    payload = parserInstance().parseTemplateArguments(messageTemplate, *args)
+}
+
+fun KLogger.traceTemplate(messageTemplate: String, vararg args: Any) = atTraceTemplate(null, messageTemplate, *args)
+
+fun KLogger.traceTemplate(throwable: Throwable, messageTemplate: String, vararg args: Any) = atTraceTemplate(throwable, messageTemplate, *args)
+
+private fun KLogger.atTraceTemplate(throwable: Throwable?, messageTemplate: String, vararg args: Any) = atTrace {
+    message = messageTemplate
+    cause = throwable
+    payload = parserInstance().parseTemplateArguments(messageTemplate, *args)
+}
+
+fun KLogger.warnTemplate(messageTemplate: String, vararg args: Any) = atWarnTemplate(null, messageTemplate, *args)
+
+fun KLogger.warnTemplate(throwable: Throwable, messageTemplate: String, vararg args: Any) = atWarnTemplate(throwable, messageTemplate, *args)
+
+private fun KLogger.atWarnTemplate(throwable: Throwable?, messageTemplate: String, vararg args: Any) = atWarn {
+    message = messageTemplate
+    cause = throwable
+    payload = parserInstance().parseTemplateArguments(messageTemplate, *args)
+}
+
+fun KLogger.errorTemplate(messageTemplate: String, vararg args: Any) = atErrorTemplate(null, messageTemplate, *args)
+
+fun KLogger.errorTemplate(throwable: Throwable, messageTemplate: String, vararg args: Any) = atErrorTemplate(throwable, messageTemplate, *args)
+
+private fun KLogger.atErrorTemplate(throwable: Throwable?, messageTemplate: String, vararg args: Any) = atError {
+    message = messageTemplate
+    cause = throwable
+    payload = parserInstance().parseTemplateArguments(messageTemplate, *args)
 }
--- src/test/kotlin/com/willwolfram18/extensions/kotlinlogging/KLoggerExtensionsTest.kt
+++ src/test/kotlin/com/willwolfram18/extensions/kotlinlogging/KLoggerExtensionsTest.kt
@@ -2,7 +2,7 @@ package com.willwolfram18.extensions.kotlinlogging
 
 import io.github.oshai.kotlinlogging.*
 import io.kotest.matchers.*
-import io.kotest.matchers.types.shouldBeSameInstanceAs
+import io.kotest.matchers.types.*
 import io.mockk.*
 import org.junit.jupiter.api.*
 
@@ -66,6 +66,165 @@ class KLoggerExtensionsTest {
 
         // Assert: the correct log method was called
         verify { logger.atDebug(any()) }
+
+        // Assert: the delegate is set properly
+        eventBuilderSlot.isCaptured shouldBe true
+
+        // Assert: invoke the captured delegate and verify arguments are set
+        eventBuilderSlot.captured.invoke(logEventBuilder)
+        verify(exactly = 1) {
+            logEventBuilder.message = "test {firstArg}"
+
+            payloadCapture.captured shouldBe mapOf("firstArg" to "arg")
+
+            causeCapture.captured shouldBeSameInstanceAs throwable
+        }
+    }
+
+    @Test
+    fun `traceTemplate without throwable should call atTrace with correct parameters`() {
+        // Arrange: the logger to run on trace level
+        every { logger.atTrace(capture(eventBuilderSlot)) } just Runs
+
+        // Act
+        logger.traceTemplate("test {firstArg}", "arg")
+
+        // Assert: the correct log method was called
+        verify { logger.atTrace(any()) }
+
+        // Assert: the delegate is set properly
+        eventBuilderSlot.isCaptured shouldBe true
+
+        // Assert: invoke the captured delegate and verify arguments are set
+        eventBuilderSlot.captured.invoke(logEventBuilder)
+        verify(exactly = 1) {
+            logEventBuilder.message = "test {firstArg}"
+
+            payloadCapture.captured shouldBe mapOf("firstArg" to "arg")
+
+            causeCapture.captured shouldBe null
+        }
+    }
+
+    @Test
+    fun `traceTemplate with throwable should call atTrace`() {
+        // Arrange: the logger to run on trace level
+        every { logger.atTrace(capture(eventBuilderSlot)) } just Runs
+
+        // Arrange: the throwable
+        val throwable = Exception("foobar")
+
+        // Act
+        logger.traceTemplate(throwable, "test {firstArg}", "arg")
+
+        // Assert: the correct log method was called
+        verify { logger.atTrace(any()) }
+
+        // Assert: the delegate is set properly
+        eventBuilderSlot.isCaptured shouldBe true
+
+        // Assert: invoke the captured delegate and verify arguments are set
+        eventBuilderSlot.captured.invoke(logEventBuilder)
+        verify(exactly = 1) {
+            logEventBuilder.message = "test {firstArg}"
+
+            payloadCapture.captured shouldBe mapOf("firstArg" to "arg")
+
+            causeCapture.captured shouldBeSameInstanceAs throwable
+        }
+    }
+
+    @Test
+    fun `warnTemplate without throwable should call atWarn with correct parameters`() {
+        // Arrange: the logger to run on warn level
+        every { logger.atWarn(capture(eventBuilderSlot)) } just Runs
+
+        // Act
+        logger.warnTemplate("test {firstArg}", "arg")
+
+        // Assert: the correct log method was called
+        verify { logger.atWarn(any()) }
+
+        // Assert: the delegate is set properly
+        eventBuilderSlot.isCaptured shouldBe true
+
+        // Assert: invoke the captured delegate and verify arguments are set
+        eventBuilderSlot.captured.invoke(logEventBuilder)
+        verify(exactly = 1) {
+            logEventBuilder.message = "test {firstArg}"
+
+            payloadCapture.captured shouldBe mapOf("firstArg" to "arg")
+
+            causeCapture.captured shouldBe null
+        }
+    }
+
+    @Test
+    fun `warnTemplate with throwable should call atWarn`() {
+        // Arrange: the logger to run on warn level
+        every { logger.atWarn(capture(eventBuilderSlot)) } just Runs
+
+        // Arrange: the throwable
+        val throwable = Exception("foobar")
+
+        // Act
+        logger.warnTemplate(throwable, "test {firstArg}", "arg")
+
+        // Assert: the correct log method was called
+        verify { logger.atWarn(any()) }
+
+        // Assert: the delegate is set properly
+        eventBuilderSlot.isCaptured shouldBe true
+
+        // Assert: invoke the captured delegate and verify arguments are set
+        eventBuilderSlot.captured.invoke(logEventBuilder)
+        verify(exactly = 1) {
+            logEventBuilder.message = "test {firstArg}"
+
+            payloadCapture.captured shouldBe mapOf("firstArg" to "arg")
+
+            causeCapture.captured shouldBeSameInstanceAs throwable
+        }
+    }
+
+    @Test
+    fun `errorTemplate without throwable should call atError with correct parameters`() {
+        // Arrange: the logger to run on error level
+        every { logger.atError(capture(eventBuilderSlot)) } just Runs
+
+        // Act
+        logger.errorTemplate("test {firstArg}", "arg")
+
+        // Assert: the correct log method was called
+        verify { logger.atError(any()) }
+
+        // Assert: the delegate is set properly
+        eventBuilderSlot.isCaptured shouldBe true
+
+        // Assert: invoke the captured delegate and verify arguments are set
+        eventBuilderSlot.captured.invoke(logEventBuilder)
+        verify(exactly = 1) {
+            logEventBuilder.message = "test {firstArg}"
+
+            payloadCapture.captured shouldBe mapOf("firstArg" to "arg")
+
+            causeCapture.captured shouldBe null
+        }
+    }
+
+    @Test
+    fun `errorTemplate with throwable should call atError`() {
+        // Arrange: the logger to run on error level
+        every { logger.atError(capture(eventBuilderSlot)) } just Runs
+
+        // Arrange: the throwable
+        val throwable = Exception("foobar")
+
+        // Act
+        logger.errorTemplate(throwable, "test {firstArg}", "arg")
+
+        // Assert: the correct log method was called
+        verify { logger.atError(any()) }
 
         // Assert: the delegate is set properly
         eventBuilderSlot.isCaptured shouldBe true
